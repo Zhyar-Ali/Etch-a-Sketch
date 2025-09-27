36825367aa5acd5b478217757f0dd8bb07e756dd
Zhyar <zhiarali595@gmail.com>
2025-09-27 23:40:17 +0300

    make the sketch fully functional

---
+++ scripts.js
@@ -0,0 +1,55 @@
+let row;
+let col;
+const CONTAINER = document.querySelector(".container");
+const button = document.querySelector("#gridLayout");
+button.addEventListener("click", gridLayout);
+
+
+function gridLayout(){
+    deleteDiv();
+    let grid = prompt("please enter the grid of your liking (eg. 16x16)");
+    while (!grid.toLowerCase().includes("x")){
+        grid = prompt("That is not a correct format to type a grid. Please use the same format as the example (eg. 16x16)");
+    }
+    size = grid.toLowerCase().split("x");
+
+    row = Number(size[0]);
+    col = Number(size[1]);
+
+    if (row>100 && col>100){
+        row=100;
+        col=100;
+    }else if (row>100){
+        row=100;
+    }else if(col>100){
+        col=100;
+    }
+    createDiv();
+    colorSquares();
+}
+
+function createDiv(){
+    for (let i=1; i<=row;i++){
+        for (let j=1;j<=col;j++){
+            var square = document.createElement("div");
+            square.classList.add("squares");
+            square.style.width = `${790/row}px`;
+            square.style.height = `${790/col}px`;
+            CONTAINER.append(square);
+        }
+    }
+}
+
+function deleteDiv(){
+    const listSquare = document.querySelectorAll(".squares");
+    listSquare.forEach(element => CONTAINER.removeChild(element));
+}
+
+function colorSquares(){
+    const listSquare = document.querySelectorAll(".squares");
+
+    listSquare.forEach(element => {
+        element.addEventListener('mouseover', () => element.style.backgroundColor="red");
+    })
+}
+
